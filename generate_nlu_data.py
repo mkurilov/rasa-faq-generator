cefb4d759e300e1104961bfdfed9226003455cba
mkurilov <72092098+mkurilov@users.noreply.github.com>
2020-12-07 19:52:55 +0300

    Update generate_nlu_data.py

--- generate_nlu_data.py
+++ generate_nlu_data.py
@@ -1,6 +1,6 @@
 from rasa_faq_generator import RasaFAQGenerator
 
 gen = RasaFAQGenerator(limit_terms_to = 100)
-gen.read_json('../data/bankiru_faq.json')
+gen.read_json('../data/bank_faq.json')
 gen.write_rasa_nlu('../data/nlu_faq.yml')
 gen.write_rasa_domain('../domain_faq.yml')
